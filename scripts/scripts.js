85c2bac1c9389c51c8230f66a0c01788a240d7f7
Blain Crawford <blaincrawford@gmail.com>
2022-01-27 17:34:38 -0500

    getting started with logic step 1

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,1 +1,21 @@
 'use strict';
+const boardSquares = document.querySelectorAll('.game-square');
+
+const Game = (function () {
+  let board = ['','','','','','','','','']
+
+  const playerMove = function (value) {
+    value = this.dataset.block
+    this.innerText = 'X'
+    board[value] = this.innerText;
+    console.log(board);
+  }
+  return {playerMove};
+})();
+
+boardSquares.forEach(square => {
+  square.addEventListener('click', Game.playerMove, false);
+});
+
+
+
