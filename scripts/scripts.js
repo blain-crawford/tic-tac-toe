e83f94ca8d2ec585f2833532dce788b3b7616223
Blain Crawford <blaincrawford@gmail.com>
2022-01-28 10:19:27 -0500

    x and o logic working, going to rework board and winning logic

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -4,19 +4,46 @@
 const Game = (function () {
   let _board = ['','','','','','','','','']
   const _boardSquares = document.querySelectorAll('.game-square');
+  const winnerBanner = document.getElementById('winner');
+  const playerOneMove = function (value) {
+    if(this.innerText === ''){
+      value = this.dataset.block;
+      this.innerText = 'X';
+      _board[value] = this.innerText;
+      console.log(_board);
+      _boardSquares.forEach(square => {
+        square.removeEventListener('click', playerOneMove, false);
+      })
+      _boardSquares.forEach(square => {
+        square.addEventListener('click', playerTwoMove, false);
+      })
+    }
+  };
 
-  const playerMove = function (value) {
-    value = this.dataset.block
-    this.innerText = 'X'
-    _board[value] = this.innerText;
-    console.log(_board);
-  }
+  const playerTwoMove = function (value) {
+    if(this.innerText === '') {
+    value = this.dataset.block;
+      this.innerText = 'O';
+      _board[value] = this.innerText;
+      console.log(_board);
+      _boardSquares.forEach(square => {
+        square.removeEventListener('click', playerTwoMove, false);
+      })
+      _boardSquares.forEach(square => {
+        square.addEventListener('click', playerOneMove, false);
+      });
+    }
+  };
+
+  // const decideWinner = function {
+  //   if (_board[0] === 'X' &&&)
+  // }
 
   _boardSquares.forEach(square => {
-    square.addEventListener('click', playerMove, false);
+    square.addEventListener('click', playerOneMove, false);
   });
   
-  return {playerMove};
+  return {playerOneMove, playerTwoMove};
 })();
 
 
